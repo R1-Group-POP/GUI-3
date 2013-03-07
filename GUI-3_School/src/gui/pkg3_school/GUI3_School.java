d7127ea1585ef24eb5cce603486b479d9d8fb55e
Patrick <pkkann@gmail.com>
2013-03-07 10:01:42 +0100

    Project start

---
+++ GUI-3_School/src/gui/pkg3_school/GUI3_School.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.pkg3_school;
+
+/**
+ *
+ * @author Patrick
+ */
+public class GUI3_School {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
